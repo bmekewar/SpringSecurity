fd3f361fafd28348ef23b1d103aafe200bc58f33
Bala M <b_mekewar@yahoo.com>
2019-10-22 19:11:52 +0530

    h2 db configuration

---
+++ 05_CustomSpringSecurity/src/main/resources/insert.sql
@@ -0,0 +1,3 @@
+INSERT INTO users (user_id,active,username, password) values (1,true,'user', 'password');
+INSERT INTO users (user_id,active,username, password) values (2,true,'admin', 'admin');
+INSERT INTO users (user_id,active,username, password) values (2,true,'test', 'test');
